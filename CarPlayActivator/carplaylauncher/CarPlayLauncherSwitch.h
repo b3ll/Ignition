0f32235b449c53b4279dcdbfdad5fbd71f96257e
Pigi Galdi <pigi.galdi@gmail.com>
2014-12-08 10:51:44 +0100

    FlipSwitch! :D

---
+++ CarPlayActivator/carplaylauncher/CarPlayLauncherSwitch.h
@@ -0,0 +1,19 @@
+//
+//  CarPlayLauncherSwitch.h
+//  CarPlayLauncher
+//
+//  Created by Pigi Galdi on 08.12.2014.
+//  Copyright (c) 2014 Pigi Galdi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#import "FSSwitchDataSource.h"
+#import "FSSwitchPanel.h"
+
+#import "../PrivateHeaders.h"
+
+@interface CarPlayLauncherSwitch : NSObject <FSSwitchDataSource>
+
+@end
